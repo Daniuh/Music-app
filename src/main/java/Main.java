2d76a6092006a711160f4f16d3e175040bef14a2
Geraldine <Gerald.e2309@gmail.com>
2022-03-28 21:36:50 -0500

    Captura y visualización de una canción manualmente

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
-    Songs safeAndSound = new Songs("", 1, "", "", 1,  "");
+
+    static Scanner read = new Scanner(System.in);
+
+    public static Songs createSong(){
+        System.out.println("Ingrese el título de la canción: " );
+        String title = read.nextLine();
+
+        System.out.println("Ingrese el género de la canción: " );
+        String gender = read.nextLine();
+
+        System.out.println("Ingrese la portada de la canción: " );
+        String cover = read.nextLine();
+
+        System.out.println("Ingrese la descripción de la canción: " );
+        String description = read.nextLine();
+
+        System.out.println("Ingrese la fecha de creación de la canción: (int)" );
+        Integer date = read.nextInt();
+
+        Songs safeAndSound = new Songs(title, 1, gender, cover, date,  description);
+
+        return safeAndSound;
+    }
+
+    public static void getSong(Songs safeAndSound){
+        System.out.println(safeAndSound.toString());
+    }
+
+    public static void main(String[] args) {
+
+        Songs safeAndSound = createSong();
+        getSong(safeAndSound);
+
+    }
 }
+
+
--- src/main/java/Songs.java
+++ src/main/java/Songs.java
@@ -11,11 +11,22 @@ public class Songs {
 
     public Songs(String title, int id, String gender, String cover, Integer date, String description){
         this.title = title;
-        this.id = id;
+        this.id = 1;
         this.gender = gender; //Género
         this.cover = cover; //Título
         this.date = date; //Fecha de creación de la canción
         this.description = description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "\n Título: " + this.title +
+                "\n id: " + this.id +
+                "\n Género: " + this.gender +
+                "\n Portada: " + this.cover +
+                "\n Fecha de creación de la canción: " + this.date +
+                "\n Descripción: " + this.description;
     }
 }
 
